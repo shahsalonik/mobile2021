319c6aea224a716dd71495b3b03e712424e3b710
Saloni Shah <2023sshah@tjhsst.edu>
2021-11-04 09:31:08 -0400

    editing drawing

--- Drawing/app/src/main/java/com/shahsaloni/drawing/DrawView.java
+++ Drawing/app/src/main/java/com/shahsaloni/drawing/DrawView.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 
 public class DrawView extends View {
     private Paint p = new Paint();
-    private int y = 0, dY = 5;
+    private int y = 1000, dY = 5;
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -19,39 +19,27 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        p.setColor(Color.argb(255, 150, 75, 0));
-        canvas.drawRect(450, 900, 600, 2000, p);
+        p.setColor(Color.argb(255, 92, 64, 51));
+        canvas.drawRect(450, 1100, 600, 2000, p);
 
         p.setColor(Color.argb(255, 255, 192, 203));
-
-        for(int x = 0; x < 100; x++) {
-            int xPos = (int) (Math.random() * 700 + 200);
-            int yPos = (int) (Math.random() * 600 + 400);
-            canvas.drawCircle(xPos, yPos, 70, p);
-        }
-
-        p.setColor(Color.argb(255, 231, 83, 128));
-
-        for(int x = 0; x < 80; x++) {
-            int xPos = (int) (Math.random() * 700 + 200);
-            int yPos = (int) (Math.random() * 600 + 400);
-            canvas.drawCircle(xPos, yPos, 70, p);
-        }
-
-        p.setColor(Color.argb(200, 255, 192, 203));
-
-        for(int x = 0; x < 100; x++) {
-            int xPos = (int) (Math.random() * 700 + 200);
-            int yPos = (int) (Math.random() * 600 + 400);
-            canvas.drawCircle(xPos, yPos, 70, p);
-        }
+        canvas.drawCircle(375, 1000, 200, p);
+        canvas.drawCircle(525, 700, 200, p);
+        canvas.drawCircle(675, 1000, 200, p);
+        canvas.drawCircle(300, 800, 150, p);
+        canvas.drawCircle(700, 800, 150, p);
+        canvas.drawCircle(700, y, 70, p);
 
         p.setColor(Color.argb(255, 0,128,0));
         canvas.drawRect(0, 1800, 2000, 2000, p);
 
         p.setColor(Color.argb(255, 255, 182, 25));
         canvas.drawCircle(105, 120, 90, p);
-        
+
+        y += dY;
+        y %= getHeight();
+        invalidate();
+
     }
 
     public int getdY() {
