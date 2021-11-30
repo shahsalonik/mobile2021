afad39e42eba8833eb088e9994006c9431f3429f
Saloni Shah <2023sshah@tjhsst.edu>
2021-11-30 14:07:01 -0500

    Working on Lab11

--- Lab11/app/src/main/java/com/shahsaloni/lab11/MainActivity.java
+++ Lab11/app/src/main/java/com/shahsaloni/lab11/MainActivity.java
@@ -1,14 +1,68 @@
 package com.shahsaloni.lab11;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    RequestQueue requestQueue;
+    String url = "https://api.quotable.io/random";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        List<Quotes> quotes = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject object = new JSONObject(response);
+                    JSONArray array = object.getJSONArray("");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject object1 = array.getJSONObject(i);
+                        String content = object1.getString("content");
+                        String author = object1.getString("author");
+                        quotes.add(new Quotes(content, author));
+                    }
+                    textView.setText("Quote: " + quotes.get(0).getQuote() + " \n\t\t- " + quotes.get(0).getAuthor());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", error.toString());
+            }
+        });
+        requestQueue.add(request);
     }
+
 }
---
+++ Lab11/app/src/main/java/com/shahsaloni/lab11/Quotes.java
@@ -0,0 +1,27 @@
+package com.shahsaloni.lab11;
+
+public class Quotes {
+    private String quote, author;
+
+    public Quotes(String quote, String author) {
+        this.quote = quote;
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
